74648027cbc821ff2ae54885737a56c8ec4088ac
Tristan <tristan.vdb@simpli.org>
2023-02-04 12:30:57 +1000

    Added in routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import "./categories.styles.scss";
-import Directory from "./components/directory/directory.component";
-import "./components/category-item/category-item.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Shop from "./routes/shop/shop.component";
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}/>
+        <Route path="shop" element={<Shop />}/>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
