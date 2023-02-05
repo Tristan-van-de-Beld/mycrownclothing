8d28265953cc4a4fa90d4fa8598e8f9d3c1c5bda
Tristan <tristan.vdb@simpli.org>
2023-02-06 06:25:21 +1000

    added in routing, logo link, and shop page

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,14 +1,22 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss"
 
 const Navigation = () => {
   return (
     <Fragment>
-      <div>
-        <div>Logo</div>
+      <div className="navigation">
+        <Link className="logo-container" to={"/"}>
+          <CrownLogo className="logo"/>
+        </Link>
+
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
+          </Link>
+          <Link className="nav-link" to="/signIn">
+            SIGN IN
           </Link>
         </div>
       </div>
