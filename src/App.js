a58535ef230cee24449113316106722854206055
Tristan-van-de-Beld <108719385+Tristan-van-de-Beld@users.noreply.github.com>
2023-02-06 06:26:21 +1000

    React router (#1)
    * Added in routing
    
    * added in routing, logo link, and shop page
    
    ---------
    
    Co-authored-by: Tristan <tristan.vdb@simpli.org>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./categories.styles.scss";
-import Directory from "./components/directory/directory.component";
-import "./components/category-item/category-item.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import Shop from "./routes/shop/shop.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}/>
+        <Route path="shop" element={<Shop />}/>
+        <Route path="signIn" element={<SignIn />}/>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
