4edd84fb8d0575504a735b3e710a2a68c0fd63fe
Tristan <tristan.vdb@simpli.org>
2023-02-07 06:48:54 +1000

    Added auth

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,16 @@
+import {signInWIthGooglePopup } from '../../utils/firebase/firebase.utils'
+
 const SignIn = () =>{
+    const logGoogleUser = async () => {
+        const response = await signInWIthGooglePopup();
+        console.log(response);
+    }
     return(
         <div>
             <h1>Sign In</h1>
+            <button onClick={logGoogleUser}>
+                Sign in with Google Popup
+            </button>
         </div>
     )
 }
